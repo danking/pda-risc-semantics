1eba193ca394362068773e88133983e7310daf54
Daniel King <danking@ccs.neu.edu>
2013-05-09 14:14:14 -0400

    Add a file to collect things related to flow semantics

--- flow.rkt
+++ flow.rkt
@@ -3,15 +3,13 @@
 (require "abstract.rkt"
          "../cfa2/cfa2.rkt")
 (provide (struct-out flow-state)
-         fstate-bp-set->term-bp-set)
+         strip-flow-BP-to-node-BP)
 
 ;; a FlowValue is ...
 ;; a FlowState is a (make-flow-state AState FlowValue)
 (struct flow-state (astate flow) #:transparent)
 
-(define (fstate-bp-set->term-bp-set bpset)
-  (for/set ((bp bpset))
-    (match-define (BP (flow-state (abstract-state node1 _ _ _ _ _) _)
-                      (flow-state (abstract-state node2 _ _ _ _ _) _))
-                  bp)
-    (BP node1 node2)))
+(define strip-flow-BP-to-node-BP
+  (match-lambda ((BP (flow-state (abstract-state node1 _ _ _ _ _) _)
+                     (flow-state (abstract-state node2 _ _ _ _ _) _))
+                 (BP node1 node2))))
