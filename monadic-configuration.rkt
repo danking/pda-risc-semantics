70b635307c1fefea996932df243e95cc050f9d90
Daniel King <daniel.zidan.king@gmail.com>
2014-06-19 13:41:50 -0400

    Add a state monad definition

---
+++ monadic-configuration.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(require "monads.rkt")
+(provide (struct-out monadic-configuration)
+         init-configuration
+         run-config-monad
+         run-config-monad*
+         ConfigMonad-bind
+         ConfigMonad-return
+         ConfigMonad-creator
+         ConfigMonad-accessor)
+
+;; a [ConfigMonad X] is a [StateMonad Configuration X]
+(struct monadic-configuration (val->bits))
+(define init-configuration
+  (monadic-configuration (hash)))
+
+;; [ConfigMonad X] -> [Values X Configuration]
+(define (run-config-monad c)
+  ((ConfigMonad-accessor c) init-configuration))
+
+(define (run-config-monad* config c)
+  ((ConfigMonad-accessor c) config))
+
+;; ConfigMonad-bind :: [ConfigMonad A] -> (A -> [ConfigMonad B]) -> [ConfigMonad B]
+(define ((ConfigMonad-bind command) f)
+  (match-define (StateMonad proc) command)
+  (StateMonad (lambda (config)
+                (define-values (a config2) (proc config))
+                (match-define (StateMonad proc2) (f a))
+                (proc2 config))))
+
+(define-syntax-rule (ConfigMonad-return x)
+  (StateMonad (lambda (config) (values x config))))
+
+(define ConfigMonad-creator StateMonad)
+(define ConfigMonad-accessor StateMonad-p)
---
+++ monads.rkt
@@ -0,0 +1,68 @@
+#lang racket
+
+(require (for-syntax syntax/parse))
+(provide (all-defined-out))
+
+(struct monad (bind return creator accessor))
+(struct monad-value (value m) #:transparent)
+
+(define-syntax-rule (instantiate-monad-ops bind
+                                           return
+                                           creator
+                                           accessor
+                                           (a b) ...)
+  (begin (define-syntax-rule (a . x)
+           (b (bind return creator accessor) . x))
+         ...))
+
+;; a [StateMonad S X] is a [StateMonad (S -> [Values X S])]
+(struct StateMonad (p))
+
+(define-syntax-rule (~>~ (bind return creator accessor)
+                         ((binding expr) (binding2 expr2) ...)
+                         body)
+  (creator
+   (lambda (config)
+     (let*-values (((binding c) ((accessor expr) config))
+                   ((binding2 c) ((accessor expr2) c))
+                   ...)
+       ((accessor body) c)))))
+
+(define-syntax-rule (~> (bind return creator accessor)
+                      ((binding expr) (binding2 expr2) ...)
+                      body ...)
+  (~>~ (bind return creator accessor)
+       ((binding expr) (binding2 expr2) ...) (return (begin body ...))))
+
+(define-syntax-rule (for/set~>~ (bind return creator accessor) (sequences) body)
+  (creator
+   (lambda (config)
+     (for/fold
+         ([s (set)]
+          [c config])
+         (sequences)
+       (define-values (x new-c) ((accessor body) c))
+       (values (set-add s x) new-c)))))
+
+(define-syntax-rule (for/list~>~ (bind return creator accessor) (sequences) body)
+  (creator
+   (lambda (config)
+     (for/fold
+         ([ls '()]
+          [c config])
+         (sequences)
+       (define-values (x new-c) ((accessor body) c))
+       (values (cons x ls) new-c)))))
+
+(define-syntax-rule (mapM m f ls)
+  (sequence m (map f ls)))
+
+(define-syntax-rule (sequence (bind return creator accessor) commands)
+  (creator
+   (lambda (config)
+     (for/fold
+         ([result '()]
+          [config config])
+         ([command commands])
+       (let-values (((x config) ((accessor command) config)))
+         (values (cons x result) config))))))
